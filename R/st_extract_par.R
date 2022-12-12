4b993952cb16c34efceaa9e42e807f9a496c096a
Dongdong Kong <kongdd@users.noreply.github.com>
2022-12-12 12:15:05 +0800

    fix `st_extract_par`

--- R/st_extract_par.R
+++ R/st_extract_par.R
@@ -3,13 +3,15 @@
 #' @export
 st_extract_par <- function(lst_files, shp) {
     if (!is.list(lst_files)) lst_files = list(lst_files)
-    file <- lst_files[[1]][1] # files[1]
+    f <- lst_files[[1]][1]
 
-    blocks <- overlap(file, shp, return.data = FALSE)
+    blocks <- overlap(f, shp, return.data = FALSE)
+    
+    cl = options("cl")
     parallel::clusterExport(cl, c("blocks"), environment())
 
     lst = plyr::llply(lst_files, function(files) {
-        Ipaper::llply_par(files, function(file) { extract2::st_extract(file, blocks) })
+        Ipaper::llply_par(files, function(file) { sf.extract::st_extract(file, blocks) })
     })
     lst
 }
